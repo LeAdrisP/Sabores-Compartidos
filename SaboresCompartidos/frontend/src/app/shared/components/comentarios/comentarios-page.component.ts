d1024c80d8e084b1eed1eca0dd28abce3a25b06f
Adrian Hernandez Marin <eladr1shdzm@gmail.com>
2026-06-10 10:05:42 -0600

    Se agrego el componente reutilizable de comentarios

---
+++ SaboresCompartidos/frontend/src/app/shared/components/comentarios/comentarios-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+
+@Component({
+  selector: 'app-comentarios-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './comentarios-page.component.html',
+  styleUrls: ['./comentarios-page.component.scss']
+})
+export class ComentariosPageComponent {
+  // Inyección del historial dinámico de navegación
+  private location = inject(Location);
+
+  /**
+   * Te regresa de forma inteligente a la vista exacta de donde venías
+   */
+  volver(): void {
+    console.log('Regresando al origen de la receta...');
+    this.location.back();
+  }
+}
